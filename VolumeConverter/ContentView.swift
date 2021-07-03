2b9ae5f60f2e22a7f19e07667240de19446c827e
Xun Ruan <galaxymacos@gmail.com>
2021-07-03 16:53:13 +0800

    Initial Commit

---
+++ VolumeConverter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  VolumeConverter
+//
+//  Created by Xun Ruan on 2021/7/3.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
